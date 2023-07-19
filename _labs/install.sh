60a289af622e0c0a85bbe606310fa8f5d0b80769
Chase Bledsoe <105551586+Chaser2143@users.noreply.github.com>
2023-07-19 14:25:11 -0600

    Update install.sh to display IP Address on boot

--- _labs/install.sh
+++ _labs/install.sh
@@ -6,6 +6,8 @@ sudo apt install git zsh gdb libcamera-dev libjpeg-dev libtiff5-dev cmake libboo
 echo "------------done installing dependencies-----------"
 echo "------------installing Oh My Zsh!--------------"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+echo "------------AutoDisplay IP Address on Boot------"
+sudo sed -i '$ a\Hello World! My IP Address is \4' /etc/issue
 echo "------------setting up swapfile-----------"
 sudo dphys-swapfile swapoff
 sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
