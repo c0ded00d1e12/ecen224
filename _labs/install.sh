702f9f90a73d28a34b9bf04bced787eaebf78adb
Chase Bledsoe <chase.bledsoe5@gmail.com>
2023-07-19 10:36:39 -0600

    Untested Install Script Added w/ Lab1 Adjustments

--- _labs/install.sh
+++ _labs/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "------------installing dependencies-----------------"
 sudo apt update
-sudo apt install git zsh gdb libcamera-dev libjpeg-dev libtiff5-dev cmake libboost-program-options-dev libdrm-dev libexif-dev tmux vim -y
+sudo apt install git zsh gdb libcamera-dev libjpeg-dev libtiff5-dev cmake libbo>
 echo "------------done installing dependencies-----------"
+echo "------------installing Oh My Zsh!------------------"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/to>
 echo "------------setting up swapfile-----------"
 sudo dphys-swapfile swapoff
 sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
