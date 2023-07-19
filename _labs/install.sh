f20f37ebb225f4f96205eadd2ede5365d1fb9116
Chase Bledsoe <105551586+Chaser2143@users.noreply.github.com>
2023-07-19 15:29:04 -0600

    Update install.sh

--- _labs/install.sh
+++ _labs/install.sh
@@ -7,7 +7,7 @@ echo "------------done installing dependencies-----------"
 echo "------------installing Oh My Zsh!--------------"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -n
 #ZSH will ask if you want to change your default at the moment. Doing so interrupts the rest of this script, so I put -n and changed it right after.
-chsh -s $(which zsh)
+chsh -s $(which zsh) #this didnt work...
 echo "------------AutoDisplay IP Address on Boot------"
 sudo sed -i '$ a\Hello World! My IP Address is \\4' /etc/issue
 echo "------------setting up swapfile-----------"
