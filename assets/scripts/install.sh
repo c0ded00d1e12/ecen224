f2be0df6935e5acae666f2e00ae87cd252b57ef4
Chris Kitras <chkitras@gmail.com>
2023-07-20 10:57:13 -0600

    Update install.sh

--- assets/scripts/install.sh
+++ assets/scripts/install.sh
@@ -15,7 +15,7 @@ echo "------------------Installing Oh My Zsh!------------------"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 # Pretty sure Zsh will steal the terminal at this point, so nothing below will run
 echo "-----------Write IP Addr to Screen on Boot---------------"
-wget "https://raw.githubusercontent.com/Chaser2143/ecen224/fall_2023/assets/scripts/install.sh"
+wget "https://raw.githubusercontent.com/Chaser2143/ecen224/fall_2023/assets/scripts/ip_addr.bin"
 chmod +x ip_addr.bin
 sudo mv ip_addr.bin /opt/
 cat << EOF
