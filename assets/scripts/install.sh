83b4463f226801e95c9ea0861c65ce1509415aef
Philip Lundrigan <lundrigan@byu.edu>
2023-09-01 11:33:12 -0600

    Fix up install script

--- assets/scripts/install.sh
+++ assets/scripts/install.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-echo "-----------------Installing Dependencies-----------------"
+
+echo "----------------- Upgrading System -----------------"
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
+
+echo "----------------- Installing Dependencies -----------------"
 sudo apt install -y git zsh gdb libcamera-dev libjpeg-dev libtiff5-dev cmake libboost-program-options-dev libdrm-dev libexif-dev tmux vim
-echo "--------------Done Installing Dependencies---------------"
-echo "-------------------Setting up Swapfile-------------------"
+
+echo "------------------- Setting up Swapfile -------------------"
 sudo dphys-swapfile swapoff
 sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
-echo "--------------------Finished Swapfile--------------------"
 
-echo "------------------Installing Oh My Zsh!------------------"
+echo "------------------ Installing Oh My Zsh! ------------------"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-echo "-----------Write IP Addr to Screen on Boot---------------"
+echo "----------- Write IP Addr to Screen on Boot ---------------"
 wget "https://raw.githubusercontent.com/Chaser2143/ecen224/fall_2023/assets/scripts/ip_addr.bin"
 chmod +x ip_addr.bin
 sudo mv ip_addr.bin /opt/
@@ -31,4 +33,5 @@ EOF
 sudo mv ip_addr.service /etc/systemd/system/
 sudo systemctl enable ip_addr.service
 
-sudo chsh /bin/zsh
+echo "----------- Changing ---------------"
+chsh -s /usr/bin/zsh
